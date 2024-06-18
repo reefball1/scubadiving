958c3fe59a96813cea9186bb52ac0a23818355b8
Zachary Artman <zacharyartman@icloud.com>
2024-06-17 21:50:46 -0700

    Linked buttons to pages

--- script.js
+++ script.js
@@ -58,6 +58,47 @@ function scrollFunction() {
     updateHeaderStyles();
 }
 
+function linkToTiles(section) {
+    const element = document.getElementById(`${section}-tile`);
+    element.onclick = function() {
+        open(section);
+    };
+}
+
+function open(section) {
+    let url = '';
+    switch (section) {
+        case 'about':
+            url = './about';
+            break;
+        case 'why':
+            url = './why';
+            break;
+        case 'steps':
+            url = './steps';
+            break;
+        case 'advanced':
+            url = './advanced';
+            break;
+        case 'pricing':
+            url = './pricing';
+            break;
+        case 'contact':
+            url = './contact';
+            break;
+        default:
+            url = './';
+    }
+    window.location.href = url;
+}
+
+linkToTiles('about');
+linkToTiles('why');
+linkToTiles('steps');
+linkToTiles('advanced');
+linkToTiles('pricing');
+linkToTiles('contact');
+
 document.getElementById('explore-button').onclick = function() {
     const tilesPage = document.getElementById('tiles');
     const yOffset = -75; 
