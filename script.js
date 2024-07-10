0decd1399bd52486f86438e2016e0159375a88d7
Zachary Artman <zacharyartman@icloud.com>
2024-07-09 17:37:40 -0700

    Fixed bug where header wasn't changing to transparent if menu was open over transparent section and window width changed from < 1200 to >

--- script.js
+++ script.js
@@ -1,91 +1,104 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector("#hamburger-icon");
-  const header = document.querySelector("#header");
+document.addEventListener('DOMContentLoaded', function () {
+  function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector("#hamburger-icon");
+    const header = document.querySelector("#header");
 
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-  header.classList.toggle("open");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    header.classList.toggle("open");
 
-  updateHeaderStyles();
-}
-
-function updateHeaderStyles() {
-  const icon = document.querySelector("#hamburger-icon");
-  const hamburgerSpans = document.querySelectorAll("#hamburger-icon span");
-  const headerText = document.querySelectorAll(".website-title, .menu-links a");
-  const isMenuOpen = icon.classList.contains('open');
-  const isHeaderScrolled = document.body.scrollTop > 25 || document.documentElement.scrollTop > 25;
-  const isTransparentHeader = document.querySelectorAll("#transparent-header").length > 0;
-
-  const spanColor = (isMenuOpen || isHeaderScrolled || !isTransparentHeader) ? "black" : "white";
-  const textColor = (isMenuOpen || isHeaderScrolled || !isTransparentHeader) ? "black" : "white";
-
-  hamburgerSpans.forEach(function (span) {
-    span.style.backgroundColor = spanColor;
-  });
-
-  headerText.forEach(function (text) {
-    text.style.color = textColor;
-  });
-
-  const header = document.querySelector("#header");
-  if (isHeaderScrolled || isMenuOpen) {
-    header.style.backgroundColor = "white";
-  } else {
-    header.style.backgroundColor = "transparent";
-  }
-}
-
-document.getElementById('hamburger-icon').onclick = function () {
-  toggleMenu();
-}
-
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  const header = document.getElementById("header");
-  const hasScrolled = document.body.scrollTop > 25 || document.documentElement.scrollTop > 25;
-
-  if (hasScrolled) {
-    header.classList.add("header-scrolled");
-  } else {
-    header.classList.remove("header-scrolled");
+    updateHeaderStyles();
   }
 
-  updateHeaderStyles();
-}
+  function updateHeaderStyles() {
+    const icon = document.querySelector("#hamburger-icon");
+    const hamburgerSpans = document.querySelectorAll("#hamburger-icon span");
+    const headerText = document.querySelectorAll(".website-title, .menu-links a");
+    const isMenuOpen = icon.classList.contains('open');
+    const isHeaderScrolled = document.body.scrollTop > 25 || document.documentElement.scrollTop > 25;
+    const isTransparentHeader = document.querySelectorAll("#transparent-header").length > 0;
 
-window.onload = function () {
-  document.body.className += " loaded";
-  updateHeaderStyles();
-}
+    const spanColor = (isMenuOpen || isHeaderScrolled || !isTransparentHeader) ? "black" : "white";
+    const textColor = (isMenuOpen || isHeaderScrolled || !isTransparentHeader) ? "black" : "white";
 
+    hamburgerSpans.forEach(function (span) {
+      span.style.backgroundColor = spanColor;
+    });
 
-function linkToAccordion(accordionID) {
-  document.getElementById(accordionID).onclick = function() {
-    toggleAccordion(accordionID);
+    headerText.forEach(function (text) {
+      text.style.color = textColor;
+    });
+
+    const header = document.querySelector("#header");
+    if (isHeaderScrolled || isMenuOpen) {
+      header.style.backgroundColor = "white";
+    } else {
+      header.style.backgroundColor = "transparent";
+    }
   }
-}
 
-function toggleAccordion(buttonID) {
-  const button = document.getElementById(buttonID);
-  const content = button.nextElementSibling;
-  const arrow = button.querySelector('.arrow');
-  if (content.style.maxHeight) {
+  document.getElementById('hamburger-icon').onclick = function () {
+    toggleMenu();
+  }
+
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    const header = document.getElementById("header");
+    const hasScrolled = document.body.scrollTop > 25 || document.documentElement.scrollTop > 25;
+
+    if (hasScrolled) {
+      header.classList.add("header-scrolled");
+    } else {
+      header.classList.remove("header-scrolled");
+    }
+
+    updateHeaderStyles();
+  }
+
+  window.onload = function () {
+    document.body.className += " loaded";
+    updateHeaderStyles();
+  }
+
+
+  function linkToAccordion(accordionID) {
+    const id = document.getElementById(accordionID)
+    if (id != null) {
+      id.onclick = function () {
+        toggleAccordion(accordionID);
+      }
+    }
+  }
+
+  function toggleAccordion(buttonID) {
+    const button = document.getElementById(buttonID);
+    const content = button.nextElementSibling;
+    const arrow = button.querySelector('.arrow');
+    if (content.style.maxHeight) {
       content.style.maxHeight = null;
       arrow.classList.remove('down');
-  } else {
+    } else {
       content.style.maxHeight = content.scrollHeight + 'px';
       arrow.classList.add('down');
+    }
   }
-}
 
-linkToAccordion('mask-accordion')
-linkToAccordion('snorkel-accordion')
-linkToAccordion('fins-accordion')
-linkToAccordion('bag-accordion')
-linkToAccordion('wetsuit-accordion')
-linkToAccordion('dive-computer-accordion')
+  linkToAccordion('mask-accordion')
+  linkToAccordion('snorkel-accordion')
+  linkToAccordion('fins-accordion')
+  linkToAccordion('bag-accordion')
+  linkToAccordion('wetsuit-accordion')
+  linkToAccordion('dive-computer-accordion')
+
+  window.addEventListener('resize', function() {
+    const header = document.querySelector("#header");
+    if (window.innerWidth <= 1200 && header.classList.contains('open')) {
+      toggleMenu();
+    }
+  })
+
+})
